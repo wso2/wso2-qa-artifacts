8c1779220ebf5cd4f7e87431c7630f8d6d6020b8
Asanka Vithanage <gvasanka@gmail.com>
2016-03-01 12:37:39 +0530

    Adding Metrics monitoring sample code

---
+++ products/wso2_MSF4J/Analytics/Metrics_Monitoring/Fat_Jar_mode/HTTPMetricsDASService-MSF4J27/src/main/java/org/asanka/service/Student.java
@@ -0,0 +1,52 @@
+package org.asanka.service;
+
+/**
+ * Created by asankav on 3/1/16.
+ */
+public class Student {
+
+
+    private String nic;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    public Student() {
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
