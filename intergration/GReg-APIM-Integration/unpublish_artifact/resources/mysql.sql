08073d9b78a0cc348684e7572dad97e5c24ba879
Asanka Vithanage <gvasanka@gmail.com>
2016-03-13 08:29:00 +0530

    Removing unpublish_artifact testing artifact from the repo

--- intergration/GReg-APIM-Integration/unpublish_artifact/resources/mysql.sql
+++
@@ -1,9 +0,0 @@
-drop database regfiveREG1;
-create database regfiveREG;
-GRANT ALL ON regfiveREG1.* TO regfiveREG1@'%' IDENTIFIED BY "regfiveREG1";
-GRANT ALL ON regfiveREG1.* TO regfiveREG1@'localhost' IDENTIFIED BY "regfiveREG1";
-
-drop database regfiveUM;
-create database regfiveUM;
-GRANT ALL ON regfiveUM.* TO regfiveUM@'%' IDENTIFIED BY "regfiveUM";
-GRANT ALL ON regfiveUM.* TO regfiveUM@'localhost' IDENTIFIED BY "regfiveUM";
