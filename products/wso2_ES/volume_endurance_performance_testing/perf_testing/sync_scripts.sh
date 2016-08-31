0122a252ad04b3d4ad03266c6f80024f01fd1c98
daneshk <dknkuruppu@gmail.com>
2016-08-31 17:08:24 +0530

    add jmeter scripts for ES performance testing

--- products/wso2_ES/volume_endurance_performance_testing/perf_testing/sync_scripts.sh
+++ products/wso2_ES/volume_endurance_performance_testing/perf_testing/sync_scripts.sh
@@ -1,1 +1,1 @@
-scp -i /home/daneshk/ES-Testing/performance-testing/ES210.pem -vr ./* ubuntu@ec2-54-174-45-39.compute-1.amazonaws.com:/home/ubuntu/perf_test_es_2_1/jmeter_scripts/
+scp -i <pem-file-path> -vr ./* <destination-path>
