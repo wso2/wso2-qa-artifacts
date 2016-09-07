855f301f52d51453e8b7fce4885ffb43f472bad4
pubudup <pubudup@wso2.com>
2016-09-07 16:56:30 +0530

    Added more test cases for TTL for Zero TTL and Created test cases for topics. Updated README.me with test cases

---
+++ products/wso2_MB/combined_scenarios/MBTestCases/README.md
@@ -0,0 +1,57 @@
+# README
+
+**Author :** Pubudu D.P
+
+**Last modified by :** N/A
+
+**Related public jira :** N/A
+
+**Related testcase :** https://testlink.wso2.com/linkto.php?tprojectPrefix=MB&item=testsuite&id=59904
+
+
+###Description
+This is a simple java project created to test the functionality of Time to Live feature related to queues. For this project to work you have to make sure the following jar files are being added to your java project using desired IDE. These jar files could be found at [$MB_HOME]/client-lib/
+
+andes-client-3.1.9.jar  
+geronimo-jms_1.1_spec-1.1.0.wso2v1.jar  
+log4j-1.2.13.jar  
+slf4j-1.5.10.wso2v1.jar
+
+###Setup
+This project should work perfectly with a Message Broker 3.2.0 M2 standlone pack
+
+> Product Version: 3.2.0. M2
+
+
+### Contents 
+MBTestCases java project. (Initially created with IntellijIDEA)
+
+### Script parameters
+N/A
+
+
+### Pre-conditions
+- Checkout the project from github repo
+- Open the project with Intellij IDEA
+- Go to Build from top panel
+- Click on 'Make Project' option
+
+
+
+
+### Execution steps
+
+1. Start a standalone Message Broker 3.2.0 M2 pack 
+2. Open the class 'QueuesSendDefault'
+3. Right click on the class and select "Run" option (This should create the queue called 'DefaultQueue' and subscribe to that queue)
+4. Open the class 'QueuesReceiveDefault'
+5. Right click on the class and select "Run" option (This should create send messages to the 'DefaultQueue' with defined TTL values)
+
+Similarly you can execute other classes as well.
+
+
+### Expected Outcome
+1. A log should be printed in QueueReceiveDefault output to inform the sent messages are being received to the subscriber (You can change the TTL values and try out the behavior.)
+
+### Post-Conditions
+- N/A
---
+++ products/wso2_MB/combined_scenarios/MBTestCases/src/main/java/ttl/Queues/QueueSendDefault.java
@@ -0,0 +1,91 @@
+package ttl.Queues;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+/**
+ * Created by pubudup on 9/5/16.
+ */
+public class QueueSendDefault {
+    public static final String QPID_ICF = "org.wso2.andes.jndi.PropertiesFileInitialContextFactory";
+    private static final String CF_NAME_PREFIX = "connectionfactory.";
+    private static final String QUEUE_NAME_PREFIX = "queue.";
+    private static final String CF_NAME = "qpidConnectionfactory";
+    String userName = "admin";
+    String password = "admin";
+    private static String CARBON_CLIENT_ID = "carbon";
+    private static String CARBON_VIRTUAL_HOST_NAME = "carbon";
+    private static String CARBON_DEFAULT_HOSTNAME = "localhost";
+    private static String CARBON_DEFAULT_PORT = "5672";
+    String queueName = "DefaultQueue";
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        QueueSendDefault queueSender = new QueueSendDefault();
+        queueSender.sendMessages();
+    }
+
+    public void sendMessages() throws NamingException, JMSException {
+
+
+        Properties properties = new Properties();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, QPID_ICF);
+        properties.put(CF_NAME_PREFIX + CF_NAME, getTCPConnectionURL(userName, password));
+        properties.put(QUEUE_NAME_PREFIX + queueName, queueName);
+
+        Properties properties2 = new Properties();
+        properties2.put(Context.INITIAL_CONTEXT_FACTORY, QPID_ICF);
+        properties2.put(CF_NAME_PREFIX + CF_NAME, getTCPConnectionURL(userName, password));
+
+
+        System.out.println("getTCPConnectionURL(userName,password) = " + getTCPConnectionURL(userName, password));
+        InitialContext ctx = new InitialContext(properties);
+
+
+        // Lookup connection factory
+        QueueConnectionFactory connFactory = (QueueConnectionFactory) ctx.lookup(CF_NAME);
+        QueueConnection queueConnection = connFactory.createQueueConnection();
+        queueConnection.start();
+        QueueSession queueSession =  queueConnection.createQueueSession(false, QueueSession.AUTO_ACKNOWLEDGE );//
+
+        Queue queue = (Queue)ctx.lookup(queueName);
+
+        javax.jms.QueueSender queueSender = queueSession.createSender(queue);
+
+
+
+        //Sending messages with incremental TTL values
+
+        for (int TTL=1000; TTL<=20000; TTL=TTL+1000)
+        {
+            TextMessage textMessage = queueSession.createTextMessage("This is a message with an incremental TTL value :" + TTL + " ms.");
+            queueSender.send(textMessage,DeliveryMode.PERSISTENT,4,TTL);
+        }
+
+
+
+        queueSession.close();
+        queueConnection.close();
+
+
+    }
+
+
+    public String getTCPConnectionURL(String username, String password) {
+        // amqp://{username}:{password}@carbon/carbon?brokerlist='tcp://{hostname}:{port}'
+        return new StringBuffer()
+                .append("amqp://").append(username).append(":").append(password)
+                .append("@").append(CARBON_CLIENT_ID)
+                .append("/").append(CARBON_VIRTUAL_HOST_NAME)
+                .append("?brokerlist='tcp://").append(CARBON_DEFAULT_HOSTNAME).append(":").append(CARBON_DEFAULT_PORT).append("'")
+                .toString();
+    }
+
+
+
+
+
+
+}
