64640a922eb8141538649c1ff46ad88330823c76
pubudup <pubudup@wso2.com>
2016-09-09 11:21:15 +0530

    Added sender and receiver classes to test TTL feature for durable subscriptions

---
+++ products/wso2_MB/combined_scenarios/MBTestCases/src/main/java/ttl/DurableTopic/DurableTopicSend.java
@@ -0,0 +1,75 @@
+package ttl.DurableTopic;
+
+/**
+ * Created by pubudup on 9/9/16.
+ */
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
+
+public class DurableTopicSend {
+
+    public static final String QPID_ICF = "org.wso2.andes.jndi.PropertiesFileInitialContextFactory";
+    private static final String CF_NAME_PREFIX = "connectionfactory.";
+    private static final String CF_NAME = "qpidConnectionfactory";
+    String userName = "admin";
+    String password = "admin";
+    private static String CARBON_CLIENT_ID = "carbon";
+    private static String CARBON_VIRTUAL_HOST_NAME = "carbon";
+    private static String CARBON_DEFAULT_HOSTNAME = "localhost";
+    private static String CARBON_DEFAULT_PORT = "5672";
+    String topicName = "DurableTopic";
+    int pubMsgCount=2000;
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        DurableTopicSend topicPublisher = new DurableTopicSend();
+        topicPublisher.publishMessage();
+    }
+    public void publishMessage() throws NamingException, JMSException {
+        Properties properties = new Properties();
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, QPID_ICF);
+        properties.put(CF_NAME_PREFIX + CF_NAME, getTCPConnectionURL(userName, password));
+        System.out.println("getTCPConnectionURL(userName,password) = " + getTCPConnectionURL(userName, password));
+        InitialContext ctx = new InitialContext(properties);
+        // Lookup connection factory
+        TopicConnectionFactory connFactory = (TopicConnectionFactory) ctx.lookup(CF_NAME);
+        TopicConnection topicConnection = connFactory.createTopicConnection();
+        topicConnection.start();
+        TopicSession topicSession =
+                topicConnection.createTopicSession(false, QueueSession.AUTO_ACKNOWLEDGE);
+        // Send message
+        Topic topic = topicSession.createTopic(topicName);
+
+        for(Integer TTL=1000;TTL<=20000;TTL=TTL+1000){
+
+            // create the message to send
+
+            TextMessage message = topicSession.createTextMessage("Text Message with"+TTL+"::Published From IP::"+CARBON_DEFAULT_HOSTNAME);
+            //message.setStringProperty("msgID",TTL.toString());
+            javax.jms.TopicPublisher topicPublisher = topicSession.createPublisher(topic);
+            topicPublisher.publish(message);
+
+            topicPublisher.publish(message,DeliveryMode.PERSISTENT,4,10000);
+            topicPublisher.publish(message, DeliveryMode.PERSISTENT,4,40000);
+
+
+        }
+
+        topicSession.close();
+        topicConnection.close();
+    }
+    public String getTCPConnectionURL(String username, String password) {
+        // amqp://{username}:{password}@carbon/carbon?brokerlist='tcp://{hostname}:{port}'
+        return new StringBuffer()
+                .append("amqp://").append(username).append(":").append(password)
+                .append("@").append(CARBON_CLIENT_ID)
+                .append("/").append(CARBON_VIRTUAL_HOST_NAME)
+                .append("?brokerlist='tcp://").append(CARBON_DEFAULT_HOSTNAME).append(":").append(CARBON_DEFAULT_PORT).append("'")
+                .toString();
+    }
+
+}
